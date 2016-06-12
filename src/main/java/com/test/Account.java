2b4eb0e1f80f18bd203527cd6da90aee318c846b
anteastra <anteastra@gmail.com>
2016-06-12 12:44:01 +0700

    Lock example

--- src/main/java/com/test/Account.java
+++ src/main/java/com/test/Account.java
@@ -1,10 +1,14 @@
 package com.test;
 
+import java.util.concurrent.locks.Lock;
+import java.util.concurrent.locks.ReentrantLock;
+
 /**
  * Created by anteastra on 11.06.2016.
  */
 public class Account {
     private int balance;
+    private Lock lock = new ReentrantLock();
 
     public Account(int balance) {
         this.balance = balance;
@@ -16,6 +20,10 @@ public class Account {
 
     public void deposit(int amount) {
         balance += amount;
+    }
+
+    public Lock getLock() {
+        return lock;
     }
 
     public int getBalance() {
---
+++ src/main/java/com/test/LockExample.java
@@ -0,0 +1,61 @@
+package com.test;
+
+import java.util.concurrent.TimeUnit;
+import java.util.concurrent.locks.Lock;
+
+/**
+ * Created by anteastra on 12.06.2016.
+ *
+ * Live lock almost always. hm. How to solve it?
+ */
+public class LockExample {
+
+    private static int WAIT_SEC = 3;
+
+    public static void main(String[] args) throws InterruptedException {
+        final Account a = new Account(2000);
+        final Account b = new Account(4000);
+        new Thread(new Runnable() {
+            public void run() {
+                try {
+                    transfer(a, b, 500);
+                } catch (InterruptedException e) {
+                    e.printStackTrace();
+                }
+            }
+        }).start();
+
+        transfer(b, a, 300);
+
+        System.out.println("a: " + a.getBalance());
+        System.out.println("b: " + b.getBalance());
+    }
+
+    private static void transfer(Account from, Account to, int amount) throws InsufficientFundsException, InterruptedException {
+
+        Lock lock1 = from.getLock();
+        Lock lock2 = to.getLock();
+
+        if (lock1.tryLock(WAIT_SEC, TimeUnit.SECONDS)) {
+            try {
+                // this lock fails often
+                if (lock2.tryLock(WAIT_SEC, TimeUnit.SECONDS)) {
+                    try {
+                        if (from.getBalance() < amount) throw new InsufficientFundsException();
+                        from.withdraw(amount);
+                        to.deposit(amount);
+                    } finally {
+                        lock2.unlock();
+                    }
+                } else {
+                    System.out.println("cant lock 'to' account");
+                }
+            } finally {
+                lock1.unlock();
+            }
+        } else {
+            System.out.println("cant lock 'from' account");
+        }
+
+    }
+}
